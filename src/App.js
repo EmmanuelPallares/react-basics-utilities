7ac2d65f866f2d259368706451983be1c79d0d85
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-12-22 18:06:31 -0600

    button change the text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "String from class name",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <p>{this.state.string}</p>
+            <button
+              onClick={() => {
+                this.setState({ string: "Text Changed" });
+              }}
+            >
+              Change the string from this button
+            </button>
+            <button
+              onClick={() => {
+                this.setState({ string: "String from class name" });
+              }}
+            >
+              Reset Changes
+            </button>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
