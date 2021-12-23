33ade2445e5b34477387c62bd55225ad96ace311
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-12-22 18:17:52 -0600

    branch change the texts

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "String from class name",
+      string: "Try to change this string",
     };
   }
   render() {
@@ -16,10 +16,8 @@ export default class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
             <p>{this.state.string}</p>
+
             <button
               onClick={() => {
                 this.setState({ string: "Text Changed" });
@@ -34,14 +32,6 @@ export default class App extends Component {
             >
               Reset Changes
             </button>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
           </header>
         </div>
       </div>
